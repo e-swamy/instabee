8e71700f825095ca2a1c1d2c4f4a8cb358b4e648
e-swamy <67904774+e-swamy@users.noreply.github.com>
2024-06-06 13:57:39 +0530

    Add files via upload
    uploaded

---
+++ assets/js/custom-owl-carousel.js
@@ -0,0 +1,20 @@
+$('.auth-owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 1,
+            nav: false
+        },
+        1000: {
+            items: 1,
+            nav: false,
+            loop: false
+        }
+    }
+})
